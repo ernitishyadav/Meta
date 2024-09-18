d573794cd17251b408253766f1609730792bbbd5
Nitish Kumar <145902163+ernitishyadav@users.noreply.github.com>
2024-09-18 21:25:06 +0530

    Update Ether.sol

--- Ether.sol
+++ Ether.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity ^0.8.0;
 
 contract MyToken {
     string public tokenName = "Nitish";
-    string public tokenAbbrv = "Kumar";
+    string public tokenAbbr = "Kumar";
     uint public totalSupply = 0;
 
-    mapping (address => uint) public balances;
+    mapping(address => uint) public balances;
 
+    // Function to mint new tokens
     function mint(address _address, uint _value) public {
         totalSupply += _value;
         balances[_address] += _value;
     }
 
+    // Function to burn tokens
     function burn(address _address, uint _value) public {
-        require(balances[_address] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balances[_address] -= _value;
+        if (balances[_address] >= _value) {
+            totalSupply -= _value;
+            balances[_address] -= _value;
+        }
     }
 }
